95cb2797d95ca28e89c691f1ccf588f736e2dbbf
Ye Joo Park <subwaymatch@gmail.com>
2025-04-01 07:08:32 -0500

    WIP writing tests for local base_files

--- tests/test_file_copy_local.py
+++ tests/test_file_copy_local.py
@@ -7,6 +7,18 @@ TEST_OUTPUT_DIR = Path(__file__).resolve().parent / "test-output"
 # Create the output directory if it doesn't exist
 TEST_OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
+base_files = {
+    TEST_NOTEBOOKS_DIR / "plaintext-files" / "base1.txt": "base1.txt",
+    TEST_NOTEBOOKS_DIR
+    / "plaintext-files"
+    / "another-folder"
+    / "base2.txt": "created-folder/base2.txt",
+    TEST_NOTEBOOKS_DIR
+    / "plaintext-files"
+    / "another-folder"
+    / "base3.txt": "created-folder/another-folder/base3.txt",
+}
+
 
 def test_file_copy_local():
     item_copy_list = {
@@ -38,13 +50,16 @@ def test_file_copy_local():
     )
 
     results = grade_notebooks(
-        [item_copy_list, item_copy_dict], verbose=False, export_csv=False
+        [item_copy_list, item_copy_dict],
+        base_files=base_files,
+        verbose=False,
+        export_csv=False,
     )
 
-    assert results[0].learner_autograded_score == 30
-    assert results[0].max_total_score == 30
-    assert results[0].num_total_test_cases == 3
+    assert results[0].learner_autograded_score == 60
+    assert results[0].max_total_score == 60
+    assert results[0].num_total_test_cases == 6
 
-    assert results[1].learner_autograded_score == 20
-    assert results[1].max_total_score == 20
-    assert results[1].num_total_test_cases == 2
+    assert results[1].learner_autograded_score == 50
+    assert results[1].max_total_score == 50
+    assert results[1].num_total_test_cases == 5
